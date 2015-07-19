05aa960e40bd9165add25a7f9e67dec8f753a310
Aaron Ran An <anran.pku@gmail.com>
2015-07-19 00:35:06 -0500

    adding new version assignment 8.4

---
+++ assignment 8.4.py
@@ -0,0 +1,28 @@
+# Assignment 8.4
+
+# 8.4 Open the file romeo.txt and read it line by line. 
+# For each line, split the line into a list of words using the split() function. 
+# The program should build a list of words. 
+# For each word on each line check to see if the word is already in the list and if not append it to the list.
+# When the program completes, sort and print the resulting words in alphabetical order.
+# You can download the sample data at http://www.pythonlearn.com/code/romeo.txt
+
+import os
+
+os.chdir('/Users/aaron.an/Aaron An/MOOC/MU | Python Programming/Assignments')
+
+# fname = raw_input("Enter file name: ")
+
+fh = open('romeo.txt')
+
+lst = list()
+
+for line in fh:
+    
+    for word in line.split():
+    
+        lst.append(word)
+        
+        test = list(set(lst))
+        
+print sorted(test)
